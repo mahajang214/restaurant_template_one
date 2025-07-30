83d061a51ee4b9292031205ddee9a54ef64de21f
mahajang214 <nayanmahajan91@gmail.com>
2025-07-31 02:29:22 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import postcss from './postcss.config.js';
+import postcss from './postcss.config.js'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/restaurant_template_one/',
+  base: '/restaurant_template_one/', // ✅ Required for GitHub Pages
   css: {
     postcss
   }
