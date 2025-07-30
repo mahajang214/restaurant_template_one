39eaedcf0a7c8279b84c18d1c0fe68394191692f
mahajang214 <nayanmahajan91@gmail.com>
2025-07-31 01:39:56 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/restaurant_template_one/", 
+  plugins: [react()],
+  base: '/restaurant_template_one/',
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: "/restaurant_template_one/", 
+// })
