c7437f7162712bb5d7966e1b3ebb0ff0d3bd540b
mahajang214 <nayanmahajan91@gmail.com>
2025-07-31 01:46:41 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/restaurant_template_one/',
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   base: "/restaurant_template_one/", 
-// })
